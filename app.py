fdbc489f5b736c378b1a5cdf3bc5ea278ed999f5
Seth Williams <sethwilliams@Megans-MacBook-Pro.local>
2019-01-14 15:16:35 -0500

    put normalize in, filled a bit of .py

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def index():
+    post_file = open('index.html', 'r')
+    post_html = post_file.read()
+    post_file.close()
+
+
+
+
+
+
+
+
+
+
+#This gets the html and renders it to the screen.
+
+index_file = open('index.html', 'r')
+    index_html = index_file.read()
+    index_html = index_html.replace('{{planet_list}}', planet_html)
+    index_file.close()
+
+    return index_html
